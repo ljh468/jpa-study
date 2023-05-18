8bc41c1c1f5a617ae8e9529781b69a95127d98f8
ljh468 <ljh468@naver.com>
2023-05-19 04:43:58 +0900

    엔티티 직접 노출

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.repository;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
@@ -99,12 +100,9 @@ public class OrderRepository {
              .getResultList();
   }
 
-  public List<OrderSimpleQueryDto> findOrderDtos() {
-    return em.createQuery(
-                 "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
-                     " from Order o" +
-                     " join o.member m" +
-                     " join o.delivery d", OrderSimpleQueryDto.class)
+  public List<Order> findAll() {
+    return em.createQuery("select o from Order o", Order.class)
              .getResultList();
   }
+
 }
