4e2a5de361d0b91f88df00f3a553fb5a143ef137
ljh468 <ljh468@naver.com>
2023-06-04 02:07:24 +0900

    공통인터페이스 분석

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
 @Repository
+// 생략 가능
+// 컴포넌트 스캔을 스프링 데이터 JPA가 자동으로 처리
+// JPA 예외를 스프링 예외로 변환하는 과정도 자동으로 처리
 public interface MemberRepository extends JpaRepository<Member, Long> {
 }
