2d8e3e7d0e7f39f5f2a2a9c4bff31b9b4425da72
ljh468 <ljh468@naver.com>
2023-06-12 01:41:07 +0900

    entity add

---
+++ querydsl/src/main/java/study/querydsl/entity/Member.java
@@ -0,0 +1,48 @@
+package study.querydsl.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static java.util.Objects.nonNull;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Member {
+
+  @Id @GeneratedValue
+  @Column(name = "member_id")
+  private Long id;
+
+  private String username;
+
+  private int age;
+
+  @ManyToOne(fetch = LAZY)
+  @JoinColumn(name = "team_id")
+  private Team team;
+
+  public Member(String username) {
+    this(username, 0);
+  }
+
+  public Member(String username, int age) {
+    this(username, age, null);
+  }
+
+  public Member(String username, int age, Team team) {
+    this.username = username;
+    this.age = age;
+    if (nonNull(team)) {
+      changeTeam(team);
+    }
+  }
+
+  // 연관관계 편의 메서드
+  private void changeTeam(Team team) {
+    this.team = team;
+    team.getMembers().add(this);
+  }
+}
--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -2,33 +2,42 @@ package study.querydsl;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
 
-	@Autowired
-	EntityManager em;
+  @PersistenceContext
+  EntityManager em;
 
-	@Test
-	void contextLoads() {
+  @Test
+  void contextLoads() {
 
-		Hello hello = new Hello();
-		em.persist(hello);
-		JPAQueryFactory query = new JPAQueryFactory(em);
-		QHello qHello = QHello.hello; //Querydsl Q타입 동작 확인
-		Hello result = query
-				.selectFrom(qHello)
-				.fetchOne();
-		Assertions.assertThat(result).isEqualTo(hello);
-		//lombok 동작 확인 (hello.getId())
-		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
+    Hello hello = new Hello();
+    em.persist(hello);
 
-	}
+    JPAQueryFactory query = new JPAQueryFactory(em);
+    QHello qHello = QHello.hello; // Querydsl Q타입 동작 확인
+
+    Hello result = query
+        .selectFrom(qHello)
+        .fetchOne();
+
+    assertThat(result).isEqualTo(hello);
+
+    // lombok 동작 확인 (hello.getId())
+    assertThat(result.getId()).isEqualTo(hello.getId());
+
+  }
 
 }
